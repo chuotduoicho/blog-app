421122b61a16c630d244c384c56a5d42774123b4
Duc Tai <vo1122000@gmail.com>
2022-02-19 10:50:43 +0700

    oke

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 const app = express();
-const PORT = "https://ductai-blog-app.herokuapp.com/";
+const PORT = process.env.PORT || 5000;
 
 const URI = process.env.DATABASE_URL;
 
