275ee5638c9bbaead4334b54b51d1fb04e1960da
Duc Tai <vo1122000@gmail.com>
2022-02-19 10:08:56 +0700

    update

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = "https://ductai-blog-app.herokuapp.com/";
 
 const URI = process.env.DATABASE_URL;
 
 app.use(cors());
-app.use(express.json({ limit: "30mb" }));
-app.use(express.urlencoded({ extended: true, limit: "30mb" }));
+app.use(bodyParser.json({ limit: "30mb" }));
+app.use(bodyParser.urlencoded({ extended: true, limit: "30mb" }));
 
 app.use("/posts", posts);
 
